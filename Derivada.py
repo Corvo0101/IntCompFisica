a3d26f1e2a3b0bd6bc81ab7338d01db7c78981db
Corvo0101 <42977447+Corvo0101@users.noreply.github.com>
2018-09-25 14:42:20 -0300

    Create Derivada.py

---
+++ Derivada.py
@@ -0,0 +1,35 @@
+# PROGRAMA PARA CALCULAR PRIMEIRA E SEGUNDA DERIVADA DA 
+# FUNCAO ARBITRARIA f(x)
+
+# funcao
+def f(x):
+    return np.cos(x)
+
+# primeira derivada, DFC
+def Df(f,x):
+    dx = x[1]-x[0]
+    xp = x+dx
+    xm = x-dx
+    return (f(xp)-f(xm))/(2*dx)
+
+# segunda derivada, DFC
+def D2f(f,x):
+    dx = x[1]-x[0]
+    xp = x+dx
+    xm = x-dx
+    return (f(xp)-2.*f(x) + f(xm))/(dx*dx)
+#erro
+
+def Erro(f,x):
+    e = np.sqrt((((D2f(f,x)-f(x))/f(x))**2))
+    return e
+
+
+n = 64
+x = np.linspace(0.,10.,n)
+a = f(x)
+#plt.plot(x,a)
+#plt.plot(x,Df(f,x))
+plt.plot(x,D2f(f,x), label = "D2f")
+plt.plot(x,D2f(f,x)+Erro(f,x),label = "D2f + e")
+plt.legend()
